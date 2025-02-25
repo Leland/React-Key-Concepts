a403f598d908cb075685f328ff33a565b80d34d7
Leland Clemmons <leland.clemmons@gmail.com>
2025-02-24 16:24:26 -0800

    7.1 Solution

--- src/components/Chapters/Chapter7.tsx
+++ src/components/Chapters/Chapter7.tsx
@@ -1,17 +1,24 @@
+import { useRef } from "react";
+
 export function Chapter7() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const programRef = useRef<HTMLSelectElement>(null);
+
   function formSubmitHandler(event: any) {
     event.preventDefault();
+    console.log(nameRef.current?.value);
+    console.log(programRef.current?.value);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameRef} />
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program">
+        <select id="program" ref={programRef}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
