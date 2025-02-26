7175872157caf84e02087f3f26ed63cdd99aab63
Leland Clemmons <leland.clemmons@gmail.com>
2025-02-26 12:44:15 -0800

    10.2 Scaffold form

---
+++ src/components/ReducerForm/ReducerForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import classes from "./Form.module.css";
+
+export function ReducerForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  const formIsValid = emailIsValid && passwordIsValid;
+
+  function changeEmailHandler(event: any) {
+    const value = event.target.value;
+    setEnteredEmail(value);
+    setEmailIsValid(value.includes("@"));
+  }
+
+  function changePasswordHandler(event: any) {
+    const value = event.target.value;
+    setEnteredPassword(value);
+    setPasswordIsValid(value.trim().length > 7);
+  }
+
+  function submitFormHandler(event: any) {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      alert("Invalid form inputs!");
+      return;
+    }
+
+    console.log("Good job!");
+    console.log(enteredEmail, enteredPassword);
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      <div className={classes.control}>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" onChange={changeEmailHandler} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={changePasswordHandler} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
--- src/pages/Chapter10.tsx
+++ src/pages/Chapter10.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Events } from "../components/Events/Events";
 import { MainHeader } from "../components/Events/MainHeader/MainHeader";
 import { StoreItem } from "../components/Events/types";
+import { ReducerForm } from "../components/ReducerForm/ReducerForm";
 import { CartContext } from "../stores/CartContext";
 
 export function Chapter10() {
@@ -24,12 +25,13 @@ export function Chapter10() {
 
   return (
     <>
-      <CartContext.Provider value={cartCtxValue}>
-        <MainHeader />
-        <main>
+      <main>
+        <CartContext.Provider value={cartCtxValue}>
+          <MainHeader />
           <Events />
-        </main>
-      </CartContext.Provider>
+        </CartContext.Provider>
+        <ReducerForm />
+      </main>
     </>
   );
 }
